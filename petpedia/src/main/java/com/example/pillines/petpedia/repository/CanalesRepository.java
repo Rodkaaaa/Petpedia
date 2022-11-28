6d7fc10d6cb9ea99e05769a829fed4633d9ed24c
Francisca Madariaga <fmadariagab1@gmail.com>
2022-11-28 18:56:50 -0300

    Update CanalesRepository.java

--- petpedia/src/main/java/com/example/pillines/petpedia/repository/CanalesRepository.java
+++ petpedia/src/main/java/com/example/pillines/petpedia/repository/CanalesRepository.java
@@ -9,15 +9,15 @@ import com.example.pillines.petpedia.model.Canales;
 
 public interface CanalesRepository extends JpaRepository<Canales, Integer> {
     
-    @Query(value = "SELECT * FROM canales WHERE marca = ?1", nativeQuery = true)
-    List<Canales> findAllAutoMarca(String marca);
+    @Query(value = "SELECT * FROM canales WHERE nombre_canal = ?1", nativeQuery = true)
+    List<Canales> findAllNombreCanales(String nombreCanal);
 
-    @Query(value= "SELECT * FROM car WHERE color = ?1", nativeQuery = true)
-    List<Canales> findAllAutoColor(String color);
+    @Query(value= "SELECT * FROM canales WHERE siglas = ?1", nativeQuery = true)
+    List<Canales> findAllCanalesSiglas(String siglas);
 
-    @Query(value = "SELECT * FROM car where color = ?1 AND marca = ?2", nativeQuery = true)
-    List<Canales> findAllAutoColorMarca(String color, String marca);
+    @Query(value = "SELECT * FROM canales where nombre_canal = ?1 AND siglas = ?2", nativeQuery = true)
+    List<Canales> findAllCanalesSiglas(String nombreCanal, String siglas);
 
-    @Query(value = "SELECT * FROM car c JOIN c.carsell cs", nativeQuery = true)
-    List<Canales> buscarAutosVendidos();
+    @Query(value = "SELECT * FROM canales c JOIN c.user us", nativeQuery = true)
+    List<Canales> buscarCanalesUsuario();
 }
