28d7da50faa5a643c1a829c015d59de8f9cbf5b9
Frederick Cid <frederickcid@gmail.com>
2022-11-29 11:37:49 -0300

    CRUD provincia

--- petpedia/src/main/java/com/example/pillines/petpedia/controller/ProvinciaController.java
+++ petpedia/src/main/java/com/example/pillines/petpedia/controller/ProvinciaController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.pillines.petpedia.model.Provincia;
@@ -20,7 +22,22 @@ public class ProvinciaController {
         this.provinciaService = provinciaService;
     }
 
-    @GetMapping("/Provincia/getAll")
+    @PostMapping("/provincia/save")
+    public void saveProvincia(Provincia provincia){
+        provinciaService.saveProvincia(provincia);
+    }
+
+    @PutMapping("/provincia/update")
+    public void updateProvincia(Provincia provincia){
+        provinciaService.saveProvincia(provincia);
+    }
+
+    @PostMapping("/provincia/delete")
+    public void saveProvincia(Integer id){
+        provinciaService.deleteProvincia(id);
+    }
+
+    @GetMapping("/provincia/getAll")
     public List<Provincia> findAll(){
         return provinciaService.findAll2();
     }
