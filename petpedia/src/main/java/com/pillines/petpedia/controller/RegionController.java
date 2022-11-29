c52664882cd913d83094545688ad0bee0d9cb8ae
Frederick Cid <frederickcid@gmail.com>
2022-11-29 17:22:55 -0300

    Terminando de editar y arreglar pom y entidades

--- petpedia/src/main/java/com/pillines/petpedia/controller/RegionController.java
+++ petpedia/src/main/java/com/pillines/petpedia/controller/RegionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pillines.petpedia.model.Region;
@@ -26,7 +27,7 @@ public class RegionController {
         return regionService.findAll2();
     }
     @PostMapping("/region/save")
-    public void saveRegion(Region region){
+    public void saveRegion(@RequestBody Region region){
         regionService.saveRegion(region);
     }
 }
