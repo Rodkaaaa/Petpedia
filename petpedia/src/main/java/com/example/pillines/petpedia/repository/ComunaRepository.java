e2e99a948cd0568adee487be473adfe6fcffddfa
Francisca Madariaga <fmadariagab1@gmail.com>
2022-11-29 11:26:08 -0300

    repocomuna
    comunas

--- petpedia/src/main/java/com/example/pillines/petpedia/repository/ComunaRepository.java
+++ petpedia/src/main/java/com/example/pillines/petpedia/repository/ComunaRepository.java
@@ -1,5 +1,20 @@
 package com.example.pillines.petpedia.repository;
 
-public class ComunaRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.pillines.petpedia.model.Comuna;
+
+public interface ComunaRepository extends CrudRepository<Comuna,Integer>{
     
+    @Query(value = "SELECT * FROM comuna WHERE nombre_comuna = ?1", nativeQuery = true)
+    List<Comuna> findAllNombreComunas(String nombreComuna);
+    
+    @Query(value = "SELECT * FROM comuna", nativeQuery = true)
+    List<Comuna> findAllComunas();
+    
+
 }
