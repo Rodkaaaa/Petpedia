0c0f99b0905917238bb074b124bbf96c7f920ee7
Frederick Cid <frederickcid@gmail.com>
2022-11-29 00:50:49 -0300

    Update RegionRepository.java

--- petpedia/src/main/java/com/example/pillines/petpedia/repository/RegionRepository.java
+++ petpedia/src/main/java/com/example/pillines/petpedia/repository/RegionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.pillines.petpedia.model.Region;
-
+public interface RegionRepository extends CrudRepository<Region,Integer>{
     
     @Query(value = "SELECT * FROM region WHERE nombre_region = ?1", nativeQuery = true)
     List<Region> findAllNombreRegion(String nombreRegion);
@@ -22,7 +22,7 @@ import com.example.pillines.petpedia.model.Region;
     @Query(value = "SELECT * FROM region r JOIN r.provincia pr", nativeQuery = true)
     List<Region> buscarRegionProvincia();
     
-    public interface RegionRepository extends CrudRepository<Region,Integer>{
+    
     @Query(value = "SELECT * FROM region", nativeQuery = true)
     List<Region> findAllRegions();
     
